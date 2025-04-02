ca965d24a86e0ba5f96138945d337f168d89c208
raiza r <rahmanraiza@gmail.com>
2025-04-02 17:58:49 +0700

    feat(post-detail): update layout

--- src/components/post/post-detail/PostDetail.tsx
+++ src/components/post/post-detail/PostDetail.tsx
@@ -2,6 +2,8 @@
 
 import { PostDetailProps } from '@/types/post';
 import PacmanLoading from '@/components/shared/loading/PacmanLoading';
+import Image from 'next/image';
+import portrait from '../../../../public/portrait.jpg';
 
 import styles from './PostDetail.module.css';
 
@@ -15,26 +17,35 @@ export default function PostDetail({ post }: { post: PostDetailProps | null }) {
   }
 
   return (
-    <>
-      <div className={styles.container}>
-        <div
-          // @ts-expect-error ignore the html issue
-          dangerouslySetInnerHTML={{ __html: post?.content }}
-          className={styles.content}
+    <div className={styles.container}>
+      <div className={styles.authorContainer}>
+        <Image
+          src={portrait}
+          alt="portrait"
+          width={48}
+          height={48}
+          quality={50}
+          className={styles.portrait}
         />
-        <div className={styles.divider} />
         <div className={styles.infoContainer}>
-          <p className={styles.date}>
-            {`Posted on `}
+          <p className={styles.author}>Posted by Raiza R</p>
+          <p className={styles.posted}>
+            {`on `}
             {/* @ts-expect-error ignore this i forgot why i use this but yeah it prevents the error */}
             {new Date(post?.created_at).toLocaleDateString('en-US', {
               day: '2-digit',
               month: 'short',
               year: 'numeric',
             })}
+            {`, on Gist`}
           </p>
         </div>
       </div>
-    </>
+      <div
+        // @ts-expect-error ignore the html issue
+        dangerouslySetInnerHTML={{ __html: post?.content }}
+        className={styles.content}
+      />
+    </div>
   );
 }
